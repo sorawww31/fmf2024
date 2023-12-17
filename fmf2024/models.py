7bef81ce06f96455441e2077ae1ea33276ac461c
sorawww31 <ee217092@meiji.ac.jp>
2023-12-17 13:16:40 +0900

    管理者画面での編集が可能になった

--- fmf2024/models.py
+++ fmf2024/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 class StudioTable(models.Model):
     name = models.CharField(max_length = 15, verbose_name = '予約者氏名')
-    band_or_individual = models.CharField(max_length = 5, choices = ((1, 'バンド練習'),(2, '個人練習')))
-    date = models.DateField(verbose_name = '日付')
+    part = models.CharField(max_length=5, choices = (('vo','Vo'), ('gt','Gt'), ('ba','Ba'), ('dr','Dr'), ('key', 'Key'), ('other','その他')), verbose_name = '予約者のパート')
+    band_or_individual = models.CharField(max_length = 5, choices = (('Band', 'バンド練習'),('indi', '個人練習')), verbose_name = '個人練習orバンド練習')
+    date = models.DateField(verbose_name = '予約日')
+    modified = models.DateTimeField(auto_now = True, verbose_name = '予約完了した時刻')
     #grade = models.CharField(choices =((1,'1年生'),(2, '2年生'),(2,'2年生'),(3,'3年生'),(4,'その他')), verbose_name = '学年')
-    period = models.CharField(max_length = 3, choices = ((1,'1限'),(2,'2限'),(3,'昼休み'),(4,'3限'),(5,'4限'),(6,'5限'),(7,'6限'),(8,'7限')))
+    period = models.CharField(verbose_name = '予約時限', max_length = 5, choices = (('1p','1限'),('2p','2限'),('break','昼休み'),('3p','3限'),('4p','4限'),('5p','5限'),('6p','6限'),('7p','7限')))
 
     def __str__(self):
-        return self.title
+        return self.name
