e5b87770506fe42c8817e2ad606f3003646d62cf
sorawww31 <ee217092@meiji.ac.jp>
2023-12-17 12:25:35 +0900

    クラス名を修正

--- fmf2024/apps.py
+++ fmf2024/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Fmf2024Config(AppConfig):
+class FMF2024Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fmf2024'
