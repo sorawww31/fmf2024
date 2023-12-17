1b6767d2b6e3f4c2a266be5854b561c068a5ceff
sorawww31 <ee217092@meiji.ac.jp>
2023-12-17 12:19:26 +0900

    strの設定

--- fmf2024/models.py
+++ fmf2024/models.py
@@ -8,3 +8,6 @@ class StudioTable(models.Model):
     date = models.DateField(verbose_name = '日付')
     #grade = models.CharField(choices =((1,'1年生'),(2, '2年生'),(2,'2年生'),(3,'3年生'),(4,'その他')), verbose_name = '学年')
     period = models.CharField(choices = ((1,'1限'),(2,'2限'),(3,'昼休み'),(4,'3限'),(5,'4限'),(6,'5限'),(7,'6限'),(8,'7限')))
+
+    def __str__(self):
+        return self.title
