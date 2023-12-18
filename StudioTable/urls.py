c6781cc6eba584ad017b34537b3864890c43560d
sorawww31 <ee217092@meiji.ac.jp>
2023-12-18 22:09:44 +0900

    全然エラー治らん！

--- StudioTable/urls.py
+++ StudioTable/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TableList
+from .views import TableList, DetailView, TableDetail
 
 urlpatterns = [
     path("", TableList.as_view(), name="list"),
+    path("detail/<int:pk>", TableDetail.as_view(), name="detail"),
 ]
--- StudioTable/views.py
+++ StudioTable/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import StudioTable
 # Create your views here.
 
 class TableList(ListView):
     model = StudioTable
     context_object_name = "reserves"
+
+class TableDetail(DetailView):
+    model = StudioTable
+    context_object_name = "reserve_detail"
