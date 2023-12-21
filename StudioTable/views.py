8315c32aaf11cb37130d444bc97fecfc953a0067
sorawww31 <ee217092@meiji.ac.jp>
2023-12-21 10:06:14 +0900

    表示のための編集

--- StudioTable/views.py
+++ StudioTable/views.py
@@ -16,3 +16,8 @@ class ReserveCreate(CreateView):
     model = ReserveTable
     fields = "__all__"
     success_url = reverse_lazy("list")
+
+def separateStudioTable(request):
+    # 日付と時限でソートされたすべてのStudioTableオブジェクトを取得
+    studio_tables = StudioTable.objects.all().order_by('date', 'period')
+    return render(request, 'studio_table.html', {'studio_tables': studio_tables})
