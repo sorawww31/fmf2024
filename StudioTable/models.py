031e338a39b58919ac19b9df76fa67c0aab3dc20
sorawww31 <ee217092@meiji.ac.jp>
2023-12-31 21:53:53 +0900

    とりあえずプッシュ

--- StudioTable/models.py
+++ StudioTable/models.py
@@ -1,23 +1,13 @@
 from django.db import models
 
 # Create your models here.
-
-class ReserveTable(models.Model):
-    name = models.CharField(max_length = 15, verbose_name = '予約名')
-
-    part_band = models.CharField(default = '',max_length=5, choices = (('バンド練習','バンド練習'),('Vo','Vo'), ('Gt','Gt'), ('Ba','Ba'), ('Fr','Dr'), ('Key', 'Key'), ('その他','その他')), verbose_name = '予約者のパートorバンド練習')
-
-    modified = models.DateTimeField(auto_now = True, verbose_name = '予約完了した時刻')
-    def __str__(self):
-        return f"{self.part_band}/{self.name}"
 class StudioTable(models.Model):
-    reserve = models.OneToOneField(ReserveTable, on_delete=models.CASCADE, verbose_name = '予約情報', null = True, blank = True)
-
+    
     period = models.CharField(default = '',verbose_name = '予約時限', max_length = 5, choices = (('1限','1限'),('2限','2限'),('昼休み','昼休み'),('3限','3限'),('4限','4限'),('5限','5限'),('6限','6限'),('7限','7限')))
     date = models.DateField(verbose_name = '予約日', null = True)
     
     def __str__(self):
-        return f"{self.date}({self.period}) {self.reserve}"
+        return f"{self.date}({self.period})"
     
     class Meta:
         constraints = [
@@ -26,3 +16,17 @@ class StudioTable(models.Model):
                 name="studio_unique"
             ),
         ]
+
+class ReserveTable(models.Model):
+    date_period = models.OneToOneField(StudioTable, on_delete=models.CASCADE, verbose_name = '予約日と時限', null = True, blank = True)
+
+    name = models.CharField(max_length = 15, verbose_name = '予約名')
+
+    part_band = models.CharField(default = '',max_length=5, choices = (('バンド練習','バンド練習'),('Vo','Vo'), ('Gt','Gt'), ('Ba','Ba'), ('Fr','Dr'), ('Key', 'Key'), ('その他','その他')), verbose_name = '予約者のパートorバンド練習')
+
+    modified = models.DateTimeField(auto_now = True, verbose_name = '予約完了した時刻')
+
+
+    def __str__(self):
+        return f"{self.date_period} {self.part_band}/{self.name}"
+    
